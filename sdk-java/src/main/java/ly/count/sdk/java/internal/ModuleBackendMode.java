f2b0e670e1faef3cebffcad2571b7e9581830381
ZAHID-ZAFAR\zahid <zahidzafar51@gmail.com>
2022-03-04 15:37:08 +0500

    - sessions, event, views, crash and user request added into q - request q process

--- sdk-java/src/main/java/ly/count/sdk/java/internal/ModuleBackendMode.java
+++ sdk-java/src/main/java/ly/count/sdk/java/internal/ModuleBackendMode.java
@@ -3,6 +3,8 @@ package ly.count.sdk.java.internal;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.*;
 
 public class ModuleBackendMode extends ModuleBase {
@@ -15,6 +17,10 @@ public class ModuleBackendMode extends ModuleBase {
     //in instances when the rating feature was not enabled
     //when a module is disabled, developer facing functions do nothing
     protected boolean disabledModule = false;
+
+    private int eventQSize = 0;
+    private final Queue<Request> requestQ = new LinkedList<>();
+    private final Map<String, JSONArray> eventQueues = new HashMap<>();
 
     private Tasks tasks;
     private Transport transport;
@@ -32,7 +38,6 @@ public class ModuleBackendMode extends ModuleBase {
     public void onContextAcquired(CtxCore ctx) {
         this.ctx = ctx;
         L.d("[ModuleBackendMode][onContextAcquired]");
-//        ratingWidgetTimeout = internalConfig.getRatingWidgetTimeout();
     }
 
     @Override
@@ -50,19 +55,56 @@ public class ModuleBackendMode extends ModuleBase {
 
     }
 
+    @Override
+    public void stop(CtxCore ctx, boolean clear) {
+        super.stop(ctx, clear);
+        tasks.shutdown();
+    }
+
     public void disableModule() {
         disabledModule = true;
     }
 
-    private void processRequestQ () {
+    private void processRequestQ() {
+        if (tasks.isRunning() || requestQ.size() == 0) {
+            return;
+        }
 
+        Request request = requestQ.remove();
+        tasks.run(sendRequest(request));
+    }
+
+    private Tasks.Task<Boolean> sendRequest(final Request request) {
+        return new Tasks.Task<Boolean>(Tasks.ID_STRICT) {
+            @Override
+            public Boolean call() throws Exception {
+                if (request == null) {
+                    return false;
+                } else {
+                    L.d("Preparing request: " + request);
+                    final Boolean check = SDKCore.instance.isRequestReady(request);
+                    if (check == null) {
+                        L.d("Request is not ready yet: " + request);
+                        return false;
+                    } else if (check.equals(Boolean.FALSE)) {
+                        L.d("Request won't be ready, removing: " + request);
+                        return true;
+                    } else {
+                        tasks.run(transport.send(request), new Tasks.Callback<Boolean>() {
+                            @Override
+                            public void call(Boolean result) throws Exception {
+                                L.d("Request " + request.storageId() + " sent?: " + result);
+                                processRequestQ();
+                            }
+                        });
+                        return true;
+                    }
+                }
+            }
+        };
     }
 
     public class BackendMode {
-
-        private int eventQSize = 0;
-        private final Queue<Request> requestQ = new LinkedList<>();
-        private final Map<String, JSONArray> eventQueues = new HashMap<>();
 
         public BackendMode() {
         }
@@ -84,27 +126,93 @@ public class ModuleBackendMode extends ModuleBase {
         }
 
         public void sessionBegin(String deviceID, long timestamp) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(timestamp);
+
+            final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            final int dow = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+
+
+            Request request = new Request();
+            request.params.add("device_id", deviceID);
+            request.params.add("begin_session", 1);
+
+            request.params.add("dow", dow);
+            request.params.add("hour", hour);
+            request.params.add("timestamp", timestamp);
+            request.params.add("tz", DeviceCore.dev.getTimezoneOffset());
+
+//            JSONObject metrics = new JSONObject();
+//            metrics.put("_os", "Windows 10");
+//            metrics.put("_os_version", "10.0");
+//            metrics.put("_locale", "en_GB");
+//            metrics.put("_store", "Windows 10");
+//
+//            request.params.add("metrics", metrics);
+
+            requestQ.add(request);
 
         }
 
-        public void sessionUpdate(String deviceID, Double duration, long timestamp) {
+        public void sessionUpdate(String deviceID, double duration, long timestamp) {
+            Request request = new Request();
+            request.params.add("device_id", deviceID);
+            request.params.add("session_duration", duration);
 
+            addTimeInfoIntoRequest(request, timestamp < 1 ? System.currentTimeMillis() : timestamp);
+
+            requestQ.add(request);
         }
 
         public void sessionEnd(String deviceID, String duration, long timestamp) {
+            Request request = new Request();
+            request.params.add("device_id", deviceID);
+            request.params.add("end_session", duration);
+            request.params.add("session_duration", duration);
+
+            JSONArray events = eventQueues.get(deviceID);
+            if (events != null && events.length() > 0) {
+                request.params.add("events", events);
+                eventQueues.remove(deviceID);
+            }
+
+            addTimeInfoIntoRequest(request, timestamp < 1 ? System.currentTimeMillis() : timestamp);
+
+            requestQ.add(request);
 
         }
 
         public void recordException(String deviceID, Throwable stacktrace, Map<String, String> segmentation, long timestamp) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            stacktrace.printStackTrace(pw);
 
+            recordException(deviceID, stacktrace.getMessage(), sw.toString(), segmentation, timestamp);
         }
 
         public void recordException(String deviceID, String exceptionMessage, String exceptionStacktrace, Map<String, String> segmentation, long timestamp) {
+            JSONObject crash = new JSONObject();
+            crash.put("_error", exceptionStacktrace);
+            crash.put("_custom", segmentation);
+            crash.put("_name", exceptionMessage);
+            //crash.put("_nonfatal", true);
 
+            Request request = new Request();
+            request.params.add("device_id", deviceID);
+            request.params.add("crash", crash);
+
+            addTimeInfoIntoRequest(request, timestamp < 1 ? System.currentTimeMillis() : timestamp);
+
+            requestQ.add(request);
         }
 
-        public void recordUserProperties(String deviceID, Map<String, String> userProperties) {
+        public void recordUserProperties(String deviceID, Map<String, String> userProperties, long timestamp) {
+            Request request = new Request();
+            request.params.add("device_id", deviceID);
+            request.params.add("user_details", userProperties);
 
+            addTimeInfoIntoRequest(request, timestamp < 1 ? System.currentTimeMillis() : timestamp);
+            requestQ.add(request);
         }
 
         private void recordEventInternal(String deviceID, String key, int count, double sum, double dur, Map<String, String> segmentation, long timestamp) {
@@ -122,10 +230,9 @@ public class ModuleBackendMode extends ModuleBase {
         }
 
         private JSONObject buildEventJSONObject(String key, int count, double sum, double dur, Map<String, String> segmentation, long timestamp) {
-
             Calendar calendar = Calendar.getInstance();
             calendar.setTimeInMillis(timestamp);
-            final int hour = DeviceCore.dev.getHourFromCalendar(calendar);
+            final int hour = calendar.get(Calendar.HOUR_OF_DAY);
             final int dow = calendar.get(Calendar.DAY_OF_WEEK) - 1;
 
             JSONObject jsonObject = new JSONObject();
@@ -160,8 +267,22 @@ public class ModuleBackendMode extends ModuleBase {
                 requestQ.add(request);
             }
 
+            eventQueues.clear();
 
+            processRequestQ();
         }
 
+        private void addTimeInfoIntoRequest(Request request, long timestamp) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(timestamp);
+
+            final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            final int dow = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+
+            request.params.add("dow", dow);
+            request.params.add("hour", hour);
+            request.params.add("timestamp", timestamp);
+            request.params.add("tz", DeviceCore.dev.getTimezoneOffset());
+        }
     }
 }
