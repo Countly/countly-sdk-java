dacc9da9171abaecdfeb01fb5f5fb763a56fc07c
Zahid Zafar <>
2023-01-05 18:41:10 +0500

    unit tests fixed!

--- sdk-java/src/test/java/ly/count/sdk/java/internal/LogTests.java
+++ sdk-java/src/test/java/ly/count/sdk/java/internal/LogTests.java
@@ -1,8 +1,7 @@
 package ly.count.sdk.java.internal;
 
-import junit.framework.Assert;
-
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -41,8 +40,7 @@ public class LogTests {
 
     @Test(expected = NullPointerException.class)
     public void logInit_null(){
-//        Log log = new Log(); TODO
-//        log.init(null);
+        Log log = new Log(null);
     }
 /*
     @Test
