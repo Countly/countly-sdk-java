692145dce2c7557d24fe4ceb3ddfbcac6f818b73
Arif Burak Demiray <57103426+arifBurakDemiray@users.noreply.github.com>
2023-09-15 15:32:15 +0300

    [Java] Remove old unused tests and EventsImp (#54)
    * refactor: delete commented out tests
    
    * feat: add some getters for testing
    
    * feat: basic test cases for EventImpl
    
    * doc: add comment to the test cases
    
    * feat: nonnull anno to check
    
    * feat: nonnull anno to Event

--- sdk-java/src/test/java/ly/count/sdk/java/internal/SessionImplTests.java
+++ sdk-java/src/test/java/ly/count/sdk/java/internal/SessionImplTests.java
@@ -20,39 +20,9 @@ public class SessionImplTests {
     public void setUp() throws Exception {
 
     }
+
     @Test
     public void filler() {
 
     }
-/*
-    @Test
-    public void constructor_deserialize() {
-        long targetID = 11234L;
-        SessionImpl session = new SessionImpl(ctx, targetID);
-        Assert.assertEquals(targetID, (long) session.getId());
-    }
-
-    @Test
-    public void addParams() {
-        SessionImpl session = new SessionImpl(ctx);
-        session.setPushOnChange(false);
-        Assert.assertNotNull(session.params);
-
-        StringBuilder sbParams = new StringBuilder();
-        String[] keys = new String[] { "a", "b", "c" };
-        String[] vals = new String[] { "11", "22", "33" };
-
-        sbParams.append(keys[0]).append("=").append(vals[0]);
-        session.addParam(keys[0], vals[0]);
-        Assert.assertEquals(sbParams.toString(), session.params.toString());
-
-        sbParams.append("&").append(keys[1]).append("=").append(vals[1]);
-        session.addParam(keys[1], vals[1]);
-        Assert.assertEquals(sbParams.toString(), session.params.toString());
-
-        sbParams.append("&").append(keys[2]).append("=").append(vals[2]);
-        session.addParam(keys[2], vals[2]);
-        Assert.assertEquals(sbParams.toString(), session.params.toString());
-    }
- */
 }
