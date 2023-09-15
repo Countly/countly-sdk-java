a31c35af135fa2ae9558f0e886058f29e0bf5c27
arifBurakDemiray <burakdemiray09@hotmail.com>
2023-09-15 11:37:04 +0300

    feat: get current request q

--- sdk-java/src/test/java/ly/count/sdk/java/internal/TestUtils.java
+++ sdk-java/src/test/java/ly/count/sdk/java/internal/TestUtils.java
@@ -1,7 +1,99 @@
 package ly.count.sdk.java.internal;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 public class TestUtils {
-    public String[] getCurrentRequestQueue() {
-        return new String[] {};
+
+    public TestUtils() {
+    }
+
+    /**
+     * Get current request queue from target folder
+     *
+     * @param targetFolder folder where requests are stored
+     * @param logger logger
+     * @return array of request params
+     */
+    public Map<String, String>[] getCurrentRequestQueue(File targetFolder, Log logger) {
+
+        //check whether target folder is a directory or not
+        if (!targetFolder.isDirectory()) {
+            logger.e("[TestUtils] " + targetFolder.getAbsolutePath() + " is not a directory");
+            return new HashMap[0];
+        }
+
+        //get all request files from target folder
+        File[] requestFiles = getRequestFiles(targetFolder);
+
+        //create array of request params
+        Map<String, String>[] resultMapArray = new HashMap[requestFiles.length];
+
+        for (int i = 0; i < requestFiles.length; i++) {
+            File file = requestFiles[i];
+            try {
+                //parse request params from file
+                Map<String, String> paramMap = parseRequestParams(file);
+                resultMapArray[i] = paramMap;
+            } catch (IOException e) {
+                logger.e("[TestUtils] " + e.getMessage());
+            }
+        }
+
+        return resultMapArray;
+    }
+
+    /**
+     * Get request files from target folder, sorted by last modified
+     *
+     * @param targetFolder folder where requests are stored
+     * @return array of request files sorted by last modified
+     */
+    private File[] getRequestFiles(File targetFolder) {
+
+        File[] files = targetFolder.listFiles();
+        if (files == null) {
+            return new File[0];
+        }
+
+        //stream all files from target folder and filter only request files and sort them by last modified
+        return Stream.of(files)
+            .filter(file -> file.getName().startsWith("[CLY]") && file.getName().contains("_request_"))
+            .sorted(Comparator.comparing(File::lastModified))
+            .toArray(File[]::new);
+    }
+
+    /**
+     * Parse request params from file. First line of file contains are urlencoded and
+     * separated by "&" symbol and key-value pairs are separated by "=" symbol (key=value).
+     *
+     * @param file request file
+     * @return map of request params
+     * @throws IOException if file cannot be read
+     */
+    private Map<String, String> parseRequestParams(File file) throws IOException {
+        try (Scanner scanner = new Scanner(file)) {
+            String firstLine = scanner.nextLine();
+            String urlDecodedStr = Utils.urldecode(firstLine);
+
+            if (urlDecodedStr == null) {
+                return new HashMap<>();
+            }
+
+            String[] params = urlDecodedStr.split("&");
+
+            Map<String, String> paramMap = new HashMap<>();
+            for (String param : params) {
+                String[] pair = param.split("=");
+                paramMap.put(pair[0], pair[1]);
+            }
+
+            return paramMap;
+        }
     }
 }
