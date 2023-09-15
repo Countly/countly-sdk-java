ba3105fcd8bfa9f7bf91eaed49cb5cd8c2988b1d
arifBurakDemiray <burakdemiray09@hotmail.com>
2023-09-15 15:33:51 +0300

    fix: sample app name

--- app-java/src/main/java/ly/count/java/demo/Example.java
+++ app-java/src/main/java/ly/count/java/demo/Example.java
@@ -7,9 +7,8 @@ import java.util.Scanner;
 
 import ly.count.sdk.java.Config;
 import ly.count.sdk.java.Countly;
-import ly.count.sdk.java.internal.LogCallback;
 
-public class Sample {
+public class Example {
 
     static void basicEvent() {
         Countly.api().event("Basic Event").record();
