c7ca8273c1ad8d8d3f4537b5f463039c8ddbc244
Arif Burak Demiray <57103426+arifBurakDemiray@users.noreply.github.com>
2023-09-18 17:56:27 +0300

    EventImpl - Tests refactor and added some new cases (#57)
    * refactor: event impl tests isolated
    
    * feat: event impl tests constructor cases
    
    * feat: null logger test for event impl
    
    * Making small changes to the test signatures
    
    * doc: fix comments a little
    
    ---------
    
    Co-authored-by: ArtursK <kadikis.arturs@gmail.com>

--- sdk-java/src/main/java/ly/count/sdk/java/internal/EventImpl.java
+++ sdk-java/src/main/java/ly/count/sdk/java/internal/EventImpl.java
@@ -9,25 +9,24 @@ import java.util.Map;
 
 import ly.count.sdk.java.Event;
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 /**
  * Event base class implementation
  */
 
 class EventImpl implements Event, JSONable {
-    private final EventRecorder recorder;
-    private final String key;
+    protected final EventRecorder recorder;
+    protected final String key;
 
-    private Map<String, String> segmentation;
+    protected Map<String, String> segmentation;
 
-    private int count;
-    private Double sum;
-    private Double duration;
+    protected int count;
+    protected Double sum;
+    protected Double duration;
 
-    private long timestamp;
-    private int hour;
-    private int dow;
+    protected long timestamp;
+    protected int hour;
+    protected int dow;
 
     final Log L;
 
@@ -218,14 +217,14 @@ class EventImpl implements Event, JSONable {
         return true;
     }
 
-    private static final String SEGMENTATION_KEY = "segmentation";
-    private static final String KEY_KEY = "key";
-    private static final String COUNT_KEY = "count";
-    private static final String SUM_KEY = "sum";
-    private static final String DUR_KEY = "dur";
-    private static final String TIMESTAMP_KEY = "timestamp";
-    private static final String DAY_OF_WEEK = "dow";
-    private static final String HOUR = "hour";
+    protected static final String SEGMENTATION_KEY = "segmentation";
+    protected static final String KEY_KEY = "key";
+    protected static final String COUNT_KEY = "count";
+    protected static final String SUM_KEY = "sum";
+    protected static final String DUR_KEY = "dur";
+    protected static final String TIMESTAMP_KEY = "timestamp";
+    protected static final String DAY_OF_WEEK = "dow";
+    protected static final String HOUR = "hour";
 
     /**
      * Serialize to JSON format according to Countly server requirements
