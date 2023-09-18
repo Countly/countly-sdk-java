6e1b263b0f86bd8990fbc0831ce7e1b96724c8b8
arifBurakDemiray <burakdemiray09@hotmail.com>
2023-09-18 17:14:02 +0300

    feat: module events

--- sdk-java/src/main/java/ly/count/sdk/java/internal/ModuleEvents.java
+++ sdk-java/src/main/java/ly/count/sdk/java/internal/ModuleEvents.java
@@ -1,17 +1,159 @@
 package ly.count.sdk.java.internal;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import ly.count.sdk.java.Countly;
+import org.json.JSONArray;
 
 public class ModuleEvents extends ModuleBase {
+    protected InternalConfig internalConfig = null;
+    protected CtxCore ctx = null;
+
+    //disabled is set when a empty module is created
+    //in instances when the rating feature was not enabled
+    //when a module is disabled, developer facing functions do nothing
+    protected boolean disabledModule = false;
+    protected final Queue<EventImpl> eventQueues = new ArrayDeque<>();
+
+    private ScheduledExecutorService executor = null;
 
     @Override
     public void init(InternalConfig config, Log logger) {
         super.init(config, logger);
         internalConfig = config;
+        L.d("[ModuleEvents] init: config = " + config);
+    }
+
+    @Override
+    public void onContextAcquired(CtxCore ctx) {
+        this.ctx = ctx;
+        L.d("[ModuleEvents] onContextAcquired: " + ctx.toString());
+
+        if (ctx.getConfig().getSendUpdateEachSeconds() > 0 && executor == null) {
+            executor = Executors.newScheduledThreadPool(1);
+            executor.scheduleWithFixedDelay(new Runnable() {
+                @Override
+                public void run() {
+                    addEventsToRequestQ();
+                }
+            }, ctx.getConfig().getSendUpdateEachSeconds(), ctx.getConfig().getSendUpdateEachSeconds(), TimeUnit.SECONDS);
+        }
+    }
+
+    private synchronized void addEventsToRequestQ() {
+        L.d("[ModuleEvents] addEventsToRequestQ");
+        JSONArray events = new JSONArray();
+
+        if (eventQueues.isEmpty()) {
+            return;
+        }
+
+        for (EventImpl event : eventQueues) {
+            events.put(event.toJSON(L));
+        }
+
+        Request request = new Request();
+        request.params.add("device_id", Countly.instance().getDeviceId());
+        request.params.add("events", events);
+        addTimeInfoIntoRequest(request, System.currentTimeMillis());
+        request.own(ModuleEvents.class);
+        addRequestToRequestQ(request);
+
+        eventQueues.clear();
+    }
+
+    private void addTimeInfoIntoRequest(Request request, Long timestamp) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(timestamp);
+
+        final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        final int dow = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+
+        request.params.add("dow", dow);
+        request.params.add("hour", hour);
+        request.params.add("timestamp", timestamp);
+        request.params.add("tz", Device.dev.getTimezoneOffset());
+    }
+
+    private void addRequestToRequestQ(Request request) {
+        synchronized (SDKCore.instance.lockBRQStorage) {
+            L.d("[ModuleEvents] addRequestToRequestQ");
+            if (internalConfig.getRequestQueueMaxSize() == SDKCore.instance.requestQueueMemory.size()) {
+                L.d("[ModuleEvents] addRequestToRequestQ: In Memory request queue is full, dropping oldest request: " + request.params.toString());
+                SDKCore.instance.requestQueueMemory.remove();
+            }
+
+            SDKCore.instance.requestQueueMemory.add(request);
+            SDKCore.instance.networking.check(ctx);
+        }
+    }
+
+    protected Map<String, Object> removeInvalidDataFromSegments(Map<String, Object> segments) {
+
+        if (segments == null || segments.isEmpty()) {
+            return segments;
+        }
+
+        int i = 0;
+        List<String> toRemove = new ArrayList<>();
+        for (Map.Entry<String, Object> item : segments.entrySet()) {
+            Object type = item.getValue();
+
+            boolean isValidDataType = (type instanceof Boolean
+                || type instanceof Integer
+                || type instanceof Long
+                || type instanceof String
+                || type instanceof Double
+                || type instanceof Float
+            );
+
+            if (!isValidDataType) {
+                toRemove.add(item.getKey());
+                L.w("[ModuleEvents] RemoveSegmentInvalidDataTypes: In segmentation Data type '" + type + "' of item '" + item.getValue() + "' isn't valid.");
+            }
+        }
+
+        for (String k : toRemove) {
+            segments.remove(k);
+        }
+
+        return segments;
     }
 
     private void recordEventInternal(String key, int count, double sum, Map<String, Object> segmentation, double dur) {
+        removeInvalidDataFromSegments(segmentation);
+        if (count <= 0) {
+            L.w("[ModuleEvents] recordEventInternal: Count can't be less than 1, ignoring this event.");
+            return;
+        }
 
+        if (key == null || key.isEmpty()) {
+            L.w("[ModuleEvents] recordEventInternal: Key can't be null or empty, ignoring this event.");
+            return;
+        }
+
+        Map<String, String> mappedSegmentation = new HashMap<>();
+
+        if (segmentation != null) {
+            for (Map.Entry<String, Object> entry : segmentation.entrySet()) {
+                mappedSegmentation.put(entry.getKey(), entry.getValue().toString());
+            }
+        }
+
+        EventImpl event = new EventImpl(key, count, sum, dur, mappedSegmentation, L);
+        eventQueues.add(event);
+
+        if (eventQueues.size() >= internalConfig.getEventsBufferSize()) {
+            addEventsToRequestQ();
+        }
     }
 
     private boolean startEventInternal(String key) {
@@ -38,6 +180,11 @@ public class ModuleEvents extends ModuleBase {
          * @param segmentation additional segmentation data that you want to set, leave null if you don't want to add anything
          */
         public void recordEvent(String key, int count, double sum, Map<String, Object> segmentation, double dur) {
+            if (!Countly.isInitialized()) {
+                L.e("[Events] recordEvent: Countly.instance().init must be called before recordEvent");
+                return;
+            }
+
             recordEventInternal(key, count, sum, segmentation, dur);
         }
 
