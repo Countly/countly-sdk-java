d147821ae07cf210ec08194476be02fdaf349686
arifBurakDemiray <burakdemiray09@hotmail.com>
2023-09-19 17:04:19 +0300

    feat: log infos

--- sdk-java/src/main/java/ly/count/sdk/java/internal/ModuleEvents.java
+++ sdk-java/src/main/java/ly/count/sdk/java/internal/ModuleEvents.java
@@ -38,6 +38,7 @@ public class ModuleEvents extends ModuleBase {
          * @param segmentation additional segmentation data that you want to set, leave null if you don't want to add anything
          */
         public void recordEvent(String key, int count, double sum, Map<String, Object> segmentation, double dur) {
+            L.i("[Events] recordEvent: key = " + key + ", count = " + count + ", sum = " + sum + ", segmentation = " + segmentation + ", dur = " + dur);
             recordEventInternal(key, count, sum, segmentation, dur);
         }
 
@@ -117,6 +118,7 @@ public class ModuleEvents extends ModuleBase {
          * @return true if no event with this key existed before and event is started, false otherwise
          */
         public boolean startEvent(final String key) {
+            L.i("[Events] startEvent: key = " + key);
             return startEventInternal(key);
         }
 
@@ -127,6 +129,7 @@ public class ModuleEvents extends ModuleBase {
          * @return true if event with this key has been previously started, false otherwise
          */
         public boolean endEvent(final String key) {
+            L.i("[Events] endEvent: key = " + key);
             return endEventInternal(key);
         }
 
@@ -136,6 +139,7 @@ public class ModuleEvents extends ModuleBase {
          * @return true if event with this key has been previously started, false otherwise
          **/
         public boolean cancelEvent(final String key) {
+            L.i("[Events] cancelEvent: key = " + key);
             return cancelEventInternal(key);
         }
     }
