2664bb25d1679ed94742cabe787bb85592d3dd95
arifBurakDemiray <burakdemiray09@hotmail.com>
2023-09-26 09:49:16 +0300

    doc: comments for new ir

--- sdk-java/src/main/java/ly/count/sdk/java/internal/ImmediateRequestGenerator.java
+++ sdk-java/src/main/java/ly/count/sdk/java/internal/ImmediateRequestGenerator.java
@@ -1,5 +1,17 @@
 package ly.count.sdk.java.internal;
 
+/**
+ * Interface for creating ImmediateRequestMaker
+ * a basic factory pattern implementation
+ *
+ * @see ImmediateRequestI
+ */
 public interface ImmediateRequestGenerator {
+    /**
+     * Create a new instance of ImmediateRequestI
+     * and override when needed
+     *
+     * @return new instance of ImmediateRequestI
+     */
     ImmediateRequestI createImmediateRequestMaker();
 }
--- sdk-java/src/main/java/ly/count/sdk/java/internal/ImmediateRequestI.java
+++ sdk-java/src/main/java/ly/count/sdk/java/internal/ImmediateRequestI.java
@@ -1,5 +1,17 @@
 package ly.count.sdk.java.internal;
 
 public interface ImmediateRequestI {
+
+    /**
+     * Do the work of making the request directly without waiting for the queue.
+     *
+     * @param requestData query parameters
+     * @param customEndpoint custom endpoint to use instead of the default one
+     * @param cp transport to use when making the request
+     * @param requestShouldBeDelayed whether the request should be delayed or not
+     * @param networkingIsEnabled whether networking is enabled or not
+     * @param callback callback to call when the request is done
+     * @param log logger to use
+     */
     void doWork(String requestData, String customEndpoint, Transport cp, boolean requestShouldBeDelayed, boolean networkingIsEnabled, ImmediateRequestMaker.InternalImmediateRequestCallback callback, Log log);
 }
