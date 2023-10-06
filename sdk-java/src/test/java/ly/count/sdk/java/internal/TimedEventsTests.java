24c0fd0a52c6f8dce24fd685c134d1283b1dd8f2
arifBurakDemiray <burakdemiray09@hotmail.com>
2023-10-06 09:30:13 +0300

    fix: git action

--- sdk-java/src/test/java/ly/count/sdk/java/internal/TimedEventsTests.java
+++ sdk-java/src/test/java/ly/count/sdk/java/internal/TimedEventsTests.java
@@ -1,7 +1,6 @@
 package ly.count.sdk.java.internal;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import ly.count.sdk.java.Countly;
 import ly.count.sdk.java.Event;
@@ -40,11 +39,11 @@ public class TimedEventsTests {
      * event queue should not be empty
      */
     @Test
-    public void recordEventRegularFlow_endAndRecord() throws Exception {
+    public void recordEventRegularFlow_endAndRecord() throws InterruptedException {
         recordEventRegularFlow_base(false);
     }
 
-    public void recordEventRegularFlow_base(boolean regularRecord) throws Exception {
+    public void recordEventRegularFlow_base(boolean regularRecord) throws InterruptedException {
         Countly.instance().init(TestUtils.getConfigEvents(2).setUpdateSessionTimerDelay(180));
 
         Event tEvent = Countly.instance().timedEvent("key");
@@ -72,8 +71,7 @@ public class TimedEventsTests {
             targetDuration = 1;
             tEvent.endAndRecord();
         }
-        List<EventImpl> events = TestUtils.getCurrentEQ();
 
-        throw new Exception(events.get(0).toString() + " " + targetSegm.toString() + " " + targetDuration);
+        TestUtils.validateEventInEQ("key", targetSegm, 5, 133.0, targetDuration, 0, 1);
     }
 }
