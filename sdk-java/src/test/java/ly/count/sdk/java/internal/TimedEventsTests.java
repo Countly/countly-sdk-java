d9d4d1df02a519c04d92456f4a6c1885015de4f2
arifBurakDemiray <burakdemiray09@hotmail.com>
2023-10-10 15:04:52 +0300

    fix: do not sync

--- sdk-java/src/test/java/ly/count/sdk/java/internal/TimedEventsTests.java
+++ sdk-java/src/test/java/ly/count/sdk/java/internal/TimedEventsTests.java
@@ -44,34 +44,32 @@ public class TimedEventsTests {
 
     public void recordEventRegularFlow_base(boolean regularRecord) throws InterruptedException {
         Countly.instance().init(TestUtils.getConfigEvents(2).setUpdateSessionTimerDelay(300));
-        synchronized (Countly.instance()) {
-            Event tEvent = Countly.instance().timedEvent("key");
-            tEvent.setCount(5).setSum(133).setDuration(456);
+        Event tEvent = Countly.instance().timedEvent("key");
+        tEvent.setCount(5).setSum(133).setDuration(456);
 
-            Map<String, String> segm = new HashMap<>();
-            segm.put("1", "a");
-            segm.put("5", "b");
+        Map<String, String> segm = new HashMap<>();
+        segm.put("1", "a");
+        segm.put("5", "b");
 
-            Map<String, Object> targetSegm = new HashMap<>();
-            targetSegm.put("1", "a");
-            targetSegm.put("5", "b");
+        Map<String, Object> targetSegm = new HashMap<>();
+        targetSegm.put("1", "a");
+        targetSegm.put("5", "b");
 
-            tEvent.setSegmentation(segm);
+        tEvent.setSegmentation(segm);
 
-            TestUtils.validateEQSize(0);
+        TestUtils.validateEQSize(0);
 
-            double targetDuration;
-            if (regularRecord) {
-                targetDuration = 456;
-                tEvent.record();
-            } else {
-                synchronized (tEvent) {
-                    tEvent.wait(1000);
-                }
-                targetDuration = 1;
-                tEvent.endAndRecord();
+        double targetDuration;
+        if (regularRecord) {
+            targetDuration = 456;
+            tEvent.record();
+        } else {
+            synchronized (tEvent) {
+                tEvent.wait(1000);
             }
-            TestUtils.validateEventInEQ("key", targetSegm, 5, 133.0, targetDuration, 0, 1);
+            targetDuration = 1;
+            tEvent.endAndRecord();
         }
+        TestUtils.validateEventInEQ("key", targetSegm, 5, 133.0, targetDuration, 0, 1);
     }
 }
