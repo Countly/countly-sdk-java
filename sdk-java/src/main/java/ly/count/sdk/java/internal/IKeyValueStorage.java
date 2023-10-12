1b14e4c3cf239af39d4abf9301a9eaf8c7c25bae
arifBurakDemiray <burakdemiray09@hotmail.com>
2023-10-12 14:41:22 +0300

    feat: jsn file storage

--- sdk-java/src/main/java/ly/count/sdk/java/internal/IKeyValueStorage.java
+++ sdk-java/src/main/java/ly/count/sdk/java/internal/IKeyValueStorage.java
@@ -10,6 +10,9 @@ public interface IKeyValueStorage<T, R> {
      */
     void add(T key, R value);
 
+    /**
+     * Saves changes to the disk/db/memory
+     */
     void save();
 
     /**
@@ -19,7 +22,26 @@ public interface IKeyValueStorage<T, R> {
      */
     void delete(T key);
 
+    /**
+     * Adds key-value pair and saves changes to the disk/db/memory
+     *
+     * @param key to set
+     * @param value to add
+     */
     void addAndSave(T key, R value);
 
+    /**
+     * Removes key-value pair and saves changes to the disk/db/memory
+     *
+     * @param key to remove
+     */
     void deleteAndSave(T key);
+
+    /**
+     * Returns value for the key
+     *
+     * @param key to get
+     * @return value
+     */
+    R get(T key);
 }
--- sdk-java/src/main/java/ly/count/sdk/java/internal/JsonFileStorage.java
+++ sdk-java/src/main/java/ly/count/sdk/java/internal/JsonFileStorage.java
@@ -1,41 +1,59 @@
 package ly.count.sdk.java.internal;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import org.json.JSONObject;
 
 public class JsonFileStorage implements IKeyValueStorage<String, Object> {
-
     private final JSONObject json;
+    private final File file;
     private final Log logger;
 
     public JsonFileStorage(File file, Log logger) {
         this.logger = logger;
+        this.file = file;
         this.json = readJsonFile(file);
     }
 
     @Override
     public void add(String key, Object value) {
+        logger.i("[JsonFileStorage] add, Adding key: [" + key + "], value: [" + value + "]");
         json.put(key, value);
     }
 
     @Override
     public void save() {
+        logger.i("[JsonFileStorage] save, Saving json file: [" + file.getAbsolutePath() + "]");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(json.toString());
+        } catch (IOException e) {
+            logger.e("[JsonFileStorage] save, Failed to save json file, reason: [" + e.getMessage() + "]");
+        }
     }
 
     @Override
     public void delete(String key) {
+        logger.i("[JsonFileStorage] delete, Deleting key: [" + key + "]");
         json.remove(key);
     }
 
     @Override
     public void addAndSave(String key, Object value) {
         add(key, value);
+        save();
     }
 
     @Override
     public void deleteAndSave(String key) {
         delete(key);
+        save();
+    }
+
+    @Override
+    public Object get(String key) {
+        return json.get(key);
     }
 
     private JSONObject readJsonFile(File file) {
