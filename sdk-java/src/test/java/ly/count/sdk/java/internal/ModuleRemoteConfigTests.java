76ba21893a06e16b2dcdcf15a4cfd6711580dd40
arifBurakDemiray <burakdemiray09@hotmail.com>
2023-10-30 09:51:15 +0300

    feat: init tests

--- sdk-java/src/test/java/ly/count/sdk/java/internal/ModuleRemoteConfigTests.java
+++ sdk-java/src/test/java/ly/count/sdk/java/internal/ModuleRemoteConfigTests.java
@@ -1,5 +1,30 @@
 package ly.count.sdk.java.internal;
 
+import ly.count.sdk.java.Config;
+import ly.count.sdk.java.Countly;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.mockito.Mockito.mock;
+
+@RunWith(JUnit4.class)
 public class ModuleRemoteConfigTests {
-    //TODO tests
+
+    Log L = mock(Log.class);
+
+    @Before
+    public void beforeTest() {
+        TestUtils.createCleanTestState();
+    }
+
+    @After
+    public void stop() {
+        Countly.instance().halt();
+    }
+
+    private void init(Config cc) {
+        Countly.instance().init(cc);
+    }
 }
