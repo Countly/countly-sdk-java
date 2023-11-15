8e35f43383e4b532df91c66d526de47233422cf1
Arif Burak Demiray <57103426+arifBurakDemiray@users.noreply.github.com>
2023-11-15 15:15:06 +0300

    feat: update sdk version (#169)

--- sdk-java/src/main/java/ly/count/sdk/java/Config.java
+++ sdk-java/src/main/java/ly/count/sdk/java/Config.java
@@ -197,7 +197,7 @@ public class Config {
     /**
      * Countly SDK version to be sent in HTTP requests
      */
-    protected String sdkVersion = "23.10.0";
+    protected String sdkVersion = "23.10.1";
 
     /**
      * Countly SDK version to be sent in HTTP requests
