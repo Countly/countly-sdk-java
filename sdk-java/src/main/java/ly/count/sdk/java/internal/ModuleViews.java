7da3d55d012e902194a5427c21ee6bbf3924b81d
Arif Burak Demiray <burakdemiray09@hotmail.com>
2023-12-19 10:27:56 +0300

    fix: convert to linekd hash map

--- sdk-java/src/main/java/ly/count/sdk/java/internal/ModuleViews.java
+++ sdk-java/src/main/java/ly/count/sdk/java/internal/ModuleViews.java
@@ -2,7 +2,7 @@ package ly.count.sdk.java.internal;
 
 import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -17,7 +17,7 @@ public class ModuleViews extends ModuleBase {
     boolean autoViewTracker = false;
     boolean automaticTrackingShouldUseShortName = false;
     final static String VIEW_EVENT_KEY = "[CLY]_view";
-    Map<String, ViewData> viewDataMap = new HashMap<>(); // map viewIDs to its viewData
+    Map<String, ViewData> viewDataMap = new LinkedHashMap<>(); // map viewIDs to its viewData
     String[] reservedSegmentationKeysViews = new String[] { "name", "visit", "start", "segment" };
     //interface for SDK users
     Views viewsInterface;
