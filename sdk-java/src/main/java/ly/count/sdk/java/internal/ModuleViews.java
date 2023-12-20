1913a1d307542baf1767e096f64bc040c3aa1658
Arif Burak Demiray <57103426+arifBurakDemiray@users.noreply.github.com>
2023-12-20 14:05:56 +0300

    Update ModuleViews.java

--- sdk-java/src/main/java/ly/count/sdk/java/internal/ModuleViews.java
+++ sdk-java/src/main/java/ly/count/sdk/java/internal/ModuleViews.java
@@ -188,12 +188,13 @@ public class ModuleViews extends ModuleBase {
     }
 
     void recordView(Double duration, Map<String, Object> segmentation) {
-        if (!internalConfig.sdk.hasConsentForFeature(CoreFeature.Events)) {
-            L.e("[ModuleViews] recordView, not consent for events");
+        ModuleEvents.Events events = Countly.instance().events();
+        if (events == null) {
+            L.e("[ModuleViews] recordView, events module is not initialized");
             return;
         }
 
-        Countly.instance().events().recordEvent(KEY_VIEW_EVENT, segmentation, 1, 0.0, duration);
+        events.recordEvent(KEY_VIEW_EVENT, segmentation, 1, 0.0, duration);
     }
 
     void recordViewEndEvent(ViewData vd, @Nullable Map<String, Object> filteredCustomViewSegmentation, String viewRecordingSource) {
