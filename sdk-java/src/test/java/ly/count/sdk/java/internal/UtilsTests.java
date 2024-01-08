d38ef196742d4b64859fcca69939fa91cf300967
Arif Burak Demiray <burakdemiray09@hotmail.com>
2024-01-08 15:21:08 +0300

    feat: scenario utils test

--- sdk-java/src/test/java/ly/count/sdk/java/internal/UtilsTests.java
+++ sdk-java/src/test/java/ly/count/sdk/java/internal/UtilsTests.java
@@ -559,29 +559,4 @@ public class UtilsTests {
         Assert.assertNull(Utils.readStream(null, logger));
         Assert.assertEquals(value, new String(Utils.readStream(new ByteArrayInputStream(value.getBytes()), logger)));
     }
-
-    /**
-     * "safeRandomVal"
-     * ### 001_validatingIDGenerator
-     * <p>
-     * testing the ID generator function that is used for events and views
-     * <p>
-     * Generate 2 values
-     * <p>
-     * they should be different.
-     * They should be 21 chars long.
-     * They should contain on base64 characters. first 8 one is base64 string and last 13 one is timestamp
-     *
-     * @throws NumberFormatException for parsing part 2
-     */
-    @Test
-    public void safeRandomVal_001_validatingIDGenerator() throws NumberFormatException {
-        String val1 = Utils.safeRandomVal();
-        String val2 = Utils.safeRandomVal();
-
-        Assert.assertNotEquals(val2, val1);
-
-        TestUtils.validateSafeRandomVal(val1);
-        TestUtils.validateSafeRandomVal(val2);
-    }
 }
