98f372b6dd21b7bcc2deefef871c16a840fd1dd2
Arif Burak Demiray <burakdemiray09@hotmail.com>
2024-01-10 11:14:58 +0300

    chore: naming sc

--- sdk-java/src/test/java/ly/count/sdk/java/internal/sc_MV_ManuelViewTests.java
+++ sdk-java/src/test/java/ly/count/sdk/java/internal/sc_MV_ManuelViewTests.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class ScMVManuelViewTests {
+public class sc_MV_ManuelViewTests {
     @Before
     public void beforeTest() {
         TestUtils.createCleanTestState();
--- sdk-java/src/test/java/ly/count/sdk/java/internal/sc_UA_UtilsTests.java
+++ sdk-java/src/test/java/ly/count/sdk/java/internal/sc_UA_UtilsTests.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class ScUAUtilsTests {
+public class sc_UA_UtilsTests {
 
     /**
      * <pre>
