c9aa6a7681178d99d0689b3caeb85f4d61ada13d
Arif Burak Demiray <burakdemiray09@hotmail.com>
2026-04-16 11:52:10 +0300

    feat: 24.1.5

--- sdk-java/src/main/java/ly/count/sdk/java/Config.java
+++ sdk-java/src/main/java/ly/count/sdk/java/Config.java
@@ -68,7 +68,7 @@ public class Config {
     /**
      * Countly SDK version to be sent in HTTP requests
      */
-    protected String sdkVersion = "24.1.4";
+    protected String sdkVersion = "24.1.5";
 
     /**
      * Countly SDK version to be sent in HTTP requests
